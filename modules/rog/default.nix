0462d55d761341a42abd5a4b93a59e210efbb240
mugdad1 <mugdad1@users.noreply.github.com>
2026-06-01 08:08:46 +0200

    rog/supergfxd: enable VFIO mode and all GPU switching modes

--- modules/rog/default.nix
+++ modules/rog/default.nix
@@ -2,7 +2,14 @@
 {
   services.asusd.enable = true;
 
-  services.supergfxd.enable = true;
+  services.supergfxd = {
+    enable = true;
+    settings = {
+      vfio_enable = true;
+      always_reboot = false;
+      no_logind = false;
+    };
+  };
 
   boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
 
