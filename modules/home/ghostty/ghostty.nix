e75f8e0b583e9350649254625fe2f9b27a43fad2
mugdad1 <mugdad1@users.noreply.github.com>
2026-06-01 07:57:14 +0200

    terminal: reduce font size to 14 (16 on P14s)

--- modules/home/ghostty/ghostty.nix
+++ modules/home/ghostty/ghostty.nix
@@ -10,7 +10,7 @@
         "Maple Mono"
         # "DejaVu Sans"
       ];
-      font-size = if (host == "p14s") then 20 else 16;
+      font-size = if (host == "p14s") then 16 else 14;
       font-feature = [
         "calt"
         "cv66"
