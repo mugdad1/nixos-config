d94d8d08d91b3f74d42a6038abaef900f237a4b1
mugdad1 <mugdad1@users.noreply.github.com>
2026-06-13 11:22:42 +0300

    switch rebuild from pkexec to sudo

--- modules/home/zsh/zsh_alias.nix
+++ modules/home/zsh/zsh_alias.nix
@@ -26,8 +26,8 @@
       tree = "eza --icons --tree --group-directories-first";
 
       # Nixos
-      rebuild = "TMPDIR=/var/tmp nh-notify nh os switch ~/nixos-config -H rog -e /run/wrappers/bin/pkexec";
-      update = "TMPDIR=/var/tmp nh-notify nh os switch ~/nixos-config -H rog -e /run/wrappers/bin/pkexec --update";
+      rebuild = "TMPDIR=/var/tmp nh-notify nh os switch ~/nixos-config -H rog -e sudo";
+      update = "TMPDIR=/var/tmp nh-notify nh os switch ~/nixos-config -H rog -e sudo --update";
       nft = "nh-notify nh os test";
       nc = "nh-notify nh clean all --keep 5";
       ns = "nom-shell --run zsh";
