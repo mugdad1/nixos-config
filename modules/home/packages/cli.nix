e7fdb10bac22e2983feaa60c003a71792c2b13e0
mugdad1 <mugdad1@users.noreply.github.com>
2026-06-14 05:09:14 +0300

    F

--- modules/home/packages/cli.nix
+++ modules/home/packages/cli.nix
@@ -5,35 +5,21 @@
     duf # disk information
     eza # ls replacement
     fd # find replacement
-    gping # ping with a graph
     gtrash # rm replacement, put deleted files in system trash
-    hexyl # hex viewer
-    man-pages # extra man pages
     ncdu # disk space
     ripgrep # grep replacement
     tldr
     xcp # faster cp written in Rust
 
     ## Tools / useful cli
-    opencode
-    asciinema
-    asciinema-agg
     broot # tree files view
     just # command runner (makefile like)
-    scooter # Interactive find and replace in the terminal
     swappy # snapshot editing tool
     tdf # cli pdf viewer
     yt-dlp-light
 
-    ## TUI
-    epy # ebook reader
-
-    onefetch # fetch utility for git repo
-    wavemon # monitoring for wireless network devices
-
     ## Multimedia
     imv
-    lowfi
     ## Utilities
     binutils # GNU binary tools (strings, objdump, etc.)
     entr # perform action when file change
