f55bb8fc8c1f2dfb4d573b7b9709de7af7c7ea89
mugdad <mugdad02@tutamail.com>
2026-06-28 00:19:37 +0300

    t480s: remove tlp, keep only throttled

--- hosts/t480s/default.nix
+++ hosts/t480s/default.nix
@@ -29,16 +29,6 @@
 
   services.throttled.enable = true;
 
-  services.tlp = {
-    enable = true;
-    settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      START_CHARGE_THRESH_BAT0 = "75";
-      STOP_CHARGE_THRESH_BAT0 = "80";
-    };
-  };
-
   services.logind.settings.Login = {
     HandleLidSwitch = "suspend";
     HandleLidSwitchExternalPower = "ignore";
