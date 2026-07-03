dd0ba24e8eed78f3c058e71a57d6262095908c3c
mugdad <mugdad02@tutamail.com>
2026-07-03 07:31:46 +0300

    fix pkexec wrapper: force-enable setuid wrapper (NixOS 26.11 disables it by default)

--- modules/core/security.nix
+++ modules/core/security.nix
@@ -1,8 +1,16 @@
-{...}: {
+{pkgs, lib, ...}: {
   security = {
     polkit.enable = true;
     rtkit.enable = true;
 
     pam.services.hyprlock = {};
+
+    wrappers.pkexec = {
+      enable = lib.mkForce true;
+      setuid = true;
+      owner = "root";
+      group = "root";
+      source = "${pkgs.polkit.bin}/bin/pkexec";
+    };
   };
 }
