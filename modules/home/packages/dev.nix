53636897219b1b8c365a5f6888bb184d76eb671d
mugdad <mugdad02@tutamail.com>
2026-07-12 06:34:44 +0300

    fix androidSdk undefined variable and update flake lock

--- modules/home/packages/dev.nix
+++ modules/home/packages/dev.nix
@@ -1,13 +1,4 @@
 { pkgs, lib, host, ... }: let
-  androidSdk = pkgs.androidenv.composeAndroidPackages {
-    platformVersions = [ "34" "35" "36" ];
-    buildToolsVersions = [ "34.0.0" "35.0.0" "36.0.0" ];
-    cmakeVersions = ["3.22.1"];
-    includeNDK = true;
-    ndkVersion = "28.2.13676358";
-    includeEmulator = false;
-    includeSystemImages = false;
-  };
 in {
   home.packages =
     with pkgs; [
@@ -36,13 +27,13 @@ in {
       jdk
       mesa-demos
       android-studio
-      androidSdk.androidsdk
+      androidsdk
     ];
 
   home.sessionVariables =
     { CHROME_EXECUTABLE = "zen-beta"; }
     // lib.optionalAttrs (host == "rog") {
-      ANDROID_SDK_ROOT = "${androidSdk.androidsdk}/libexec/android-sdk";
-      ANDROID_HOME = "${androidSdk.androidsdk}/libexec/android-sdk";
+      ANDROID_SDK_ROOT = "${pkgs.androidsdk}/libexec/android-sdk";
+      ANDROID_HOME = "${pkgs.androidsdk}/libexec/android-sdk";
     };
 }
