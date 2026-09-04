16f7580accb21f558af57be0ad760a72dde3cdcb
mugdad <mugdad02@tutamail.com>
2026-09-04 20:40:44 +0300

    fix: pin hplip to python313 for pyqt5 ABI compatibility

--- modules/desktop/printing.nix
+++ modules/desktop/printing.nix
@@ -2,7 +2,7 @@
   # CUPS print server + HP drivers (covers both USB/wired and network/IP printers)
   services.printing = {
     enable = true;
-    drivers = with pkgs; [hplip];
+    drivers = [pkgs.hplip.override {python3Packages = pkgs.python313Packages;}];
     # openFirewall = true;  # only if you want to print to/from other machines
   };
 
