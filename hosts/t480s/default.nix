757e71f1c2b49c0550300ca149bce79928c7d140
mugdad <mugdad02@tutamail.com>
2026-09-14 13:04:12 +0300

    t480s: raise battery charge limit to 85%

--- hosts/t480s/default.nix
+++ hosts/t480s/default.nix
@@ -37,7 +37,7 @@
     wantedBy = ["multi-user.target"];
     serviceConfig = {
       Type = "oneshot";
-      ExecStart = "${pkgs.bash}/bin/bash -c 'for bat in /sys/class/power_supply/BAT*; do [ -f \"$bat/charge_control_end_threshold\" ] && echo 80 > \"$bat/charge_control_end_threshold\"; done'";
+      ExecStart = "${pkgs.bash}/bin/bash -c 'for bat in /sys/class/power_supply/BAT*; do [ -f \"$bat/charge_control_end_threshold\" ] && echo 85 > \"$bat/charge_control_end_threshold\"; done'";
       ProtectSystem = "strict";
       PrivateTmp = true;
       NoNewPrivileges = true;
