1b98813cfacc9c34036cc1f9f174feed7388e66c
mugdad <mugdad02@tutamail.com>
2026-09-14 19:50:45 +0300

    wip: kwm/kwim packages, hash fix, remove wofi

--- packages/kwm.nix
+++ packages/kwm.nix
@@ -52,7 +52,7 @@ stdenv.mkDerivation (finalAttrs: {
   zigDeps = zig_0_16.fetchDeps {
     inherit (finalAttrs) src pname version;
     fetchAll = true;
-    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    hash = "sha256-Lz/Wcy40rxN81n/mBj4YJVbyGOolHzSFZMs93T1h0oQ=";
   };
 
   postConfigure = ''
