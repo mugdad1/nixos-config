448d838f2549c5eaef66ca03301c50f82576e69c
mugdad <mugdad02@tutamail.com>
2026-09-15 19:55:08 +0300

    t480s: switch theme to catppuccin (mocha mauve)

--- hosts/t480s/variables.nix
+++ hosts/t480s/variables.nix
@@ -12,7 +12,7 @@
   bar = "waybar";
 
   # Theme palette: gruvbox | nord | catppuccin | tokyo-night | rose-pine
-  theme = "nord";
+  theme = "catppuccin";
 
   keyboardLayout = "us,ara";
   keyboardOptions = "grp:alt_shift_toggle";
