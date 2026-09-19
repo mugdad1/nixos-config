56e790b4319cd1ed3bb6151933af87d51ee5e24c
mugdad <mugdad02@tutamail.com>
2026-09-19 14:00:32 +0300

    home/cli: add payload_dumper (Python-based OTA extractor)

--- modules/home/cli.nix
+++ modules/home/cli.nix
@@ -60,7 +60,7 @@
     wl-clipboard # wayland clipboard
     xdg-utils
     opencode
-    payload-dumper-go # extract Android OTA payload.bin
+    payload_dumper # extract Android OTA payload.bin
     ## Misc
     just # command runner
     cliamp
